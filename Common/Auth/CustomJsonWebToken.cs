9d9b92ccd22df4c69032c60a665f8a74728051cf
Juan Basalo <jbasalo@jmm-solutions.com>
2020-03-25 20:28:28 -0500

    Added JWT and JWT Handler, before doing the login

---
+++ Common/Auth/CustomJsonWebToken.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Common.Auth
+{
+    public class CustomJsonWebToken
+    {
+        public string Token { get; set; }
+        public long Expire { get; set; }
+    }
+}
---
+++ Common/Auth/JwtHandler.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Auth
+{
+    public class JwtHandler : IJwtHandler
+    {
+        private readonly JwtSecurityTokenHandler jwtSecurityTokenHandler;
+        private readonly JwtOptions jwtOptions;
+        private readonly SecurityKey issuerSigningKey;
+        private readonly SigningCredentials signingCredentials;
+        private readonly JwtHeader jwtHeader;
+        private readonly TokenValidationParameters tokenValidationParameters;
+
+        public JwtHandler(IOptions<JwtOptions> jwtOptions)
+        {
+            jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+            this.jwtOptions = jwtOptions.Value;
+            issuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(this.jwtOptions.SecretKey));
+            signingCredentials = new SigningCredentials(issuerSigningKey, SecurityAlgorithms.HmacSha256);
+            jwtHeader = new JwtHeader(signingCredentials);
+            tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateAudience = false,
+                ValidIssuer = this.jwtOptions.Issuer,
+                IssuerSigningKey = issuerSigningKey
+            };
+        }
+        public CustomJsonWebToken Create(Guid userId)
+        {
+            var nowUtc = DateTime.UtcNow;
+            var expiration = nowUtc.AddMinutes(jwtOptions.ExpiryMinutes);
+            var centuryBegin = new DateTime(1970, 1, 1).ToUniversalTime();
+            var exp = (long)(new TimeSpan(expiration.Ticks - centuryBegin.Ticks).TotalSeconds);
+            var now = (long)(new TimeSpan(nowUtc.Ticks - centuryBegin.Ticks).TotalSeconds);
+            var payload = new JwtPayload
+            {
+                {"sub", userId},
+                {"iss", jwtOptions.Issuer},
+                {"iat", now},
+                {"exp", exp},
+                {"unique_name", userId}
+            };
+            var jwt = new JwtSecurityToken(jwtHeader, payload);
+            var token = jwtSecurityTokenHandler.WriteToken(jwt);
+
+            return new CustomJsonWebToken
+            {
+                Token = token,
+                Expire = exp
+            };
+        }
+    }
+}
